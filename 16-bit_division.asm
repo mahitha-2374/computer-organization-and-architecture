cc7c0f0bdc320ae160716513059d1a5333dc3fbc
Mahitha Namdigam <123183421+mahitha-2374@users.noreply.github.com>
2024-09-17 23:03:24 +0530

    16-bit_division.asm

--- 16-bit_division.asm
+++ 16-bit_division.asm
@@ -1,7 +1,7 @@
 org 100h               ; Set origin address
 
-mov ax, 2400h          ; Load ax with the value 1780h
-mov bx, 1200h          ; Load bx with the value 1000h
+mov ax, 2400h          ; Load ax with the value 2400h
+mov bx, 1200h          ; Load bx with the value 1200h
 div bx                 ; Divide ax by bx, result in ax, remainder in dx
 
 mov bx, ax             ; Store quotient from ax into bx
